2e4b9803848179a2704802805e2637cc6d7465b7
RNA <a@b.com>
2025-10-15 21:41:50 +0200

    add lazy loading

--- view/galerie.php
+++ view/galerie.php
@@ -27,7 +27,7 @@
     <div class="gallery__block">
         <?php foreach ($args['listPhoto'] as $photo){ ?>
             <div class="gallery__block_item">
-                <img src="<?= $photo['image'] ?>" alt="<?= $photo['libelle'] ?>">
+                <img data-src="<?= $photo['image'] ?>" alt="<?= $photo['libelle'] ?>" class="lazy">
                 <?php if (isset($_SESSION['user']) && ($_SESSION['user']['role'] === 'admin')){ ?>
                     <div class="btn__admin">
                         <a href="/ctrl/galerie-delete.php?id=<?= $photo['id'] ?>&lang=<?= $lang ?>" class="btn__admin_item"><?= $language['delete_btn']?></a>          
@@ -44,4 +44,27 @@
     filtreBtn?.addEventListener('click', () => {
     categorieList.classList.toggle('show');
     });
+</script>
+
+<script>
+    const lazyImages = document.querySelectorAll("img.lazy");
+    if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.remove("lazy");
+            obs.unobserve(img);
+        }
+        });
+    });
+    lazyImages.forEach(img => observer.observe(img));
+    } else {
+    // Chargement classique si IntersectionObserver n’est pas disponible
+    lazyImages.forEach(img => {
+        img.src = img.dataset.src;
+        img.classList.remove("lazy");
+    });
+    }
 </script>
